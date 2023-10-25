f41bcd1b4b55164947ec49581308162ad37496a2
zhuyan <zy.arcane@gmail.com>
2023-10-25 21:06:26 +0800

    feat: optimize contact us

--- src/components/Home/ContactUs/index.tsx
+++ src/components/Home/ContactUs/index.tsx
@@ -1,30 +1,43 @@
 import Image from 'next/image'
-import React, { useCallback } from 'react'
+import React, { useCallback, useState } from 'react'
 
 import Button from '@/components/Button'
 
 import person from '../images/Person.svg'
 import phone from '../images/Phone.svg'
 import style from './style.module.css'
+import { cn } from '@/utils/cn'
 
 const ContactUs: React.FC = () => {
   const [name, setName] = React.useState('')
   const [contact, setContact] = React.useState('')
   const [demands, setDemands] = React.useState('')
-  const handleSend = useCallback(() => {
-    console.log('name && contact && demands: ', name, contact, demands)
+
+  const [loading, setLoading] = useState(false)
+  const [success, setSuccess] = useState(false)
+
+  const handleSend = useCallback(async () => {
     if (name && contact && demands) {
-      fetch('/api/contactus', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          name,
-          contact,
-          demands,
-        }),
-      })
+      setLoading(true)
+      try {
+        await fetch('/api/contactus', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            name,
+            contact,
+            demands,
+          }),
+        })
+        setSuccess(true)
+        setName('')
+        setContact('')
+        setDemands('')
+      } finally {
+        setLoading(false)
+      }
     }
   }, [contact, demands, name])
 
@@ -35,52 +48,67 @@ const ContactUs: React.FC = () => {
     >
       <div className={style.title}>企业工作流升级 募世AI助您一臂之力</div>
 
-      <div className={style.form}>
-        <label>
-          <Image
-            src={person}
-            alt="person"
-          />
-          <input
-            value={name}
-            onChange={(e) => {
-              setName(e.target.value)
+      <div className={cn(style.form, success ? style.success : undefined)}>
+        {success ? (
+          <div
+            className="cursor-pointer self-center text-gray-500"
+            onClick={() => {
+              setSuccess(false)
             }}
-            name="name"
-            placeholder="输入您的姓名"
-          />
-        </label>
-        <label>
-          <Image
-            src={phone}
-            alt="person"
-          />
-          <input
-            value={contact}
-            onChange={(e) => {
-              setContact(e.target.value)
-            }}
-            name="contact"
-            placeholder="输入您的联系方式"
-          />
-        </label>
-        <label>
-          <textarea
-            value={demands}
-            onChange={(e) => {
-              setDemands(e.target.value)
-            }}
-            rows={4}
-            name="demands"
-            placeholder="您的使用场景"
-          />
-        </label>
+          >
+            再次预约
+          </div>
+        ) : (
+          <>
+            <label>
+              <Image
+                src={person}
+                alt="person"
+              />
+              <input
+                value={name}
+                onChange={(e) => {
+                  setName(e.target.value)
+                }}
+                name="name"
+                placeholder="输入您的姓名"
+              />
+            </label>
+            <label>
+              <Image
+                src={phone}
+                alt="person"
+              />
+              <input
+                value={contact}
+                onChange={(e) => {
+                  setContact(e.target.value)
+                }}
+                name="contact"
+                placeholder="输入您的联系方式"
+              />
+            </label>
+            <label>
+              <textarea
+                value={demands}
+                onChange={(e) => {
+                  setDemands(e.target.value)
+                }}
+                rows={4}
+                name="demands"
+                placeholder="您的使用场景"
+              />
+            </label>
+          </>
+        )}
+
         <Button
-          disabled={!name || !contact || !demands}
+          disabled={loading || (!success && (!name || !contact || !demands))}
+          loading={loading}
           className="w-full"
-          onClick={handleSend}
+          onClick={success ? undefined : handleSend}
         >
-          预约专家交流
+          {success ? '感谢您的预约，我们将与您尽快联系' : '预约专家交流'}
         </Button>
       </div>
     </div>
